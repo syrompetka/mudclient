5807e53bf0b75d8d5bb0ef8e9a10bf8b15830fdf
petka <syrompetka@users.noreply.github.com>
2011-09-06 01:44:50 +0400

    forgotten files

---
+++ Adan.Client.Common/Commands/BuiltinCommandTypes.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BuiltInCommandTypes.cs" company="Adamand MUD">
+//   Copyright (c) Adamant MUD
+// </copyright>
+// <summary>
+//   Defines the BuiltInCommandTypes type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Adan.Client.Common.Commands
+{
+    /// <summary>
+    /// A class to store built in command types.
+    /// </summary>
+    public static class BuiltInCommandTypes
+    {
+        /// <summary>
+        /// Type of any "text" command (i.e. command typed by end-user).
+        /// </summary>
+        public const int TextCommand = 1;
+
+        /// <summary>
+        /// Type of hotkey command.
+        /// </summary>
+        public const int HotkeyCommand = 2;
+
+        /// <summary>
+        /// Type of command to connect/disconnect.
+        /// </summary>
+        public const int ConnectionCommands = 3;
+    }
+}
