1208f3b596a8c5061c8b8d6faf6bca21baa7114e
Petr <Petr@petka>
2014-02-20 02:27:55 +0400

    forgotten files

---
+++ Adan.Client/ConveyorUnits/ToggleFullScreenModeUnit.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Adan.Client.Commands;
+using Adan.Client.Common.Commands;
+using Adan.Client.Common.ConveyorUnits;
+using Adan.Client.Common.Model;
+using CSLib.Net.Annotations;
+using CSLib.Net.Diagnostics;
+
+namespace Adan.Client.ConveyorUnits
+{
+    /// <summary>
+    /// A <see cref="ConveyorUnit"/> implementation to toggle full screen mode of the main window.
+    /// </summary>
+    public sealed class ToggleFullScreenModeUnit : ConveyorUnit
+    {
+        private readonly MainWindow _mainWindow;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ToggleFullScreenModeUnit"/> class.
+        /// </summary>
+        /// <param name="mainWindow">The main window.</param>
+        public ToggleFullScreenModeUnit([NotNull] MainWindow mainWindow)
+        {
+            Assert.ArgumentNotNull(mainWindow, "mainWindow");
+            _mainWindow = mainWindow;
+        }
+
+        /// <summary>
+        /// Gets a set of message types that this unit can handle.
+        /// </summary>
+        public override IEnumerable<int> HandledMessageTypes
+        {
+            get { return Enumerable.Empty<int>(); }
+        }
+
+        /// <summary>
+        /// Gets a set of command types that this unit can handle.
+        /// </summary>
+        public override IEnumerable<int> HandledCommandTypes
+        {
+            get { return new[] { BuiltInCommandTypes.ToggleFullScreenMode, BuiltInCommandTypes.TextCommand }; }
+        }
+
+        /// <summary>
+        /// Handles the command.
+        /// </summary>
+        /// <param name="command">The command.</param>
+        /// <param name="rootModel">The root model.</param>
+        /// <param name="isImport">if set to <c>true</c> [is import].</param>
+        public override void HandleCommand(Command command, RootModel rootModel, bool isImport = false)
+        {
+            var toggleCommand = command as ToggleFullScreenModeCommand;
+            if (toggleCommand != null)
+            {
+                _mainWindow.ToggleFullScreenMode();
+                command.Handled = true;
+                return;
+            }
+
+            var textCommand = command as TextCommand;
+            if (textCommand != null)
+            {
+                if(string.Equals(textCommand.CommandText.Trim(), "#togglefullscreen", StringComparison.OrdinalIgnoreCase))
+                {
+                    _mainWindow.ToggleFullScreenMode();
+                    command.Handled = true;
+                }
+            }
+
+        }
+    }
+}
---
+++ Adan.Client/ViewModel/Actions/SendToWindowActionViewModel.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Adan.Client.Common.Model;
+using Adan.Client.Common.Plugins;
+using Adan.Client.Common.ViewModel;
+using Adan.Client.Model.Actions;
+using CSLib.Net.Annotations;
+using CSLib.Net.Diagnostics;
+
+namespace Adan.Client.ViewModel.Actions
+{
+    /// <summary>
+    /// A view model for <see cref="SendToWindowAction"/>.
+    /// </summary>
+    public sealed class SendToWindowActionViewModel : ActionViewModelBase
+    {
+        private readonly SendToWindowAction _action;
+        private readonly IEnumerable<ActionDescription> _allDescriptors;
+        private ActionsViewModel _actionsToExecute;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SendToWindowActionViewModel"/> class.
+        /// </summary>
+        /// <param name="action">The action.</param>
+        /// <param name="actionDescriptor">The action descriptor.</param>
+        /// <param name="allActionDescriptions">All action descriptions.</param>
+        public SendToWindowActionViewModel([NotNull] SendToWindowAction action, [NotNull] ActionDescription actionDescriptor, [NotNull] IEnumerable<ActionDescription> allActionDescriptions)
+            : base(action, actionDescriptor, allActionDescriptions)
+        {
+            _action = action;
+            _allDescriptors = allActionDescriptions;
+            _actionsToExecute = new ActionsViewModel(action.ActionsToExecute, allActionDescriptions);
+            _actionsToExecute.PropertyChanged += HandleActionDescriptionChange;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether specified command should be sent to all windows.
+        /// </summary>
+        public bool SendToAllWindows
+        {
+            get { return _action.SendToAllWindows; }
+            set
+            {
+                _action.SendToAllWindows = value;
+                OnPropertyChanged("SendToAllWindows");
+                OnPropertyChanged("ActionDescription");
+            }
+        }
+
+        /// <summary>
+        /// Get or set the name of window to send commands to.
+        /// </summary>
+        [CanBeNull]
+        public string OutputWindowName
+        {
+            get
+            {
+                return _action.OutputWindowName;
+            }
+
+            set
+            {
+                _action.OutputWindowName = value;
+                OnPropertyChanged("OutputWindowName");
+                OnPropertyChanged("ActionDescription");
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the actions to execute.
+        /// </summary>
+        /// <value>
+        /// The actions to execute.
+        /// </value>
+        [NotNull]
+        public ActionsViewModel ActionsToExecute
+        {
+            get
+            {
+                return _actionsToExecute;
+            }
+
+            set
+            {
+                Assert.ArgumentNotNull(value, "value");
+
+                if (_actionsToExecute != null)
+                {
+                    _actionsToExecute.PropertyChanged -= HandleActionDescriptionChange;
+                }
+
+                _actionsToExecute = value;
+                _actionsToExecute.PropertyChanged += HandleActionDescriptionChange;
+                OnPropertyChanged("ActionsToExecute");
+                OnPropertyChanged("ActionDescription");
+            }
+        }
+        /// <summary>
+        /// Gets the action description.
+        /// </summary>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public override string ActionDescription
+        {
+            get
+            {
+                if (SendToAllWindows)
+                {
+                    return "#sendall " + ActionsToExecute.ActionsDescription;
+                }
+
+                return "#send " + ActionsToExecute.ActionsDescription + " " + OutputWindowName;
+            }
+        }
+
+        /// <summary>
+        /// Clones this instance.
+        /// </summary>
+        /// <returns>
+        /// A deep copy of this instance.
+        /// </returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public override ActionViewModelBase Clone()
+        {
+            var conditionalAction = new SendToWindowAction();
+            return new SendToWindowActionViewModel(conditionalAction, ActionDescriptor, _allDescriptors)
+            {
+                OutputWindowName = OutputWindowName,
+                SendToAllWindows = SendToAllWindows,
+                ActionsToExecute = ActionsToExecute.Clone(conditionalAction.ActionsToExecute)
+            };
+        }
+
+        private void HandleActionDescriptionChange([NotNull] object sender, [NotNull] PropertyChangedEventArgs e)
+        {
+            Assert.ArgumentNotNull(sender, "sender");
+            Assert.ArgumentNotNull(e, "e");
+
+            if (e.PropertyName == "ActionsDescription")
+            {
+                OnPropertyChanged("ActionDescription");
+            }
+        }
+    }
+}
