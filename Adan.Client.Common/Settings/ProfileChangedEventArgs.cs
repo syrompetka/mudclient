ef47219a800f7e05ade313117d4fbfd9066c33ca
andrei-markeev <andrei-markeev@users.noreply.github.com>
2016-03-23 07:58:32 +0200

    forgotten file

---
+++ Adan.Client.Common/Settings/ProfileChangedEventArgs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Adan.Client.Common.Settings
+{
+    /// <summary>
+    /// Event arguments for the ProfileChanged event of ProfileHolder
+    /// </summary>
+    public sealed class ProfileChangedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name">Name of the profile</param>
+        /// <param name="globalChange">Is the change global?</param>
+        public ProfileChangedEventArgs(string name, bool globalChange = false)
+        {
+            Name = name;
+            Global = globalChange;
+        }
+
+        /// <summary>
+        /// Name of the profile that has been changed
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Is the change global?
+        /// </summary>
+        public bool Global { get; set; }
+
+    }
+}
