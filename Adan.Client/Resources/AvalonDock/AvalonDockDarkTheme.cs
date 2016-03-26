b8abebb88be215775db4a93b8fe0a0456c658942
petka <petka@localhost>
2016-03-26 22:26:30 +0300

    floating window themes

---
+++ Adan.Client/Resources/AvalonDock/AvalonDockDarkTheme.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xceed.Wpf.AvalonDock.Themes;
+
+namespace Adan.Client.Resources.AvalonDock
+{
+    public class AvalonDockDarkTheme:Theme
+    {
+        public override Uri GetResourceUri()
+        {
+            return new Uri(
+                "/Adan.Client;component/Resources/AvalonDock/DarkTheme.xaml",
+                UriKind.Relative);
+        }
+    }
+}
