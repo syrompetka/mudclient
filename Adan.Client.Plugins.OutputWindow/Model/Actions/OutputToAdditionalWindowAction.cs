406b47a0db00082a817bfdd0da185fa4d774f21f
lrdx <syrompetka@users.noreply.github.com>
2016-03-27 18:44:57 +0300

    Fix output to additional window from gui.

--- Adan.Client.Plugins.OutputWindow/Model/Actions/OutputToAdditionalWindowAction.cs
+++ Adan.Client.Plugins.OutputWindow/Model/Actions/OutputToAdditionalWindowAction.cs
@@ -111,19 +111,94 @@ namespace Adan.Client.Plugins.OutputWindow.Model.Actions
             Assert.ArgumentNotNull(model, "model");
             Assert.ArgumentNotNull(context, "context");
 
-            var textMessage = context.CurrentMessage as TextMessage;
-            if (textMessage != null)
+            string str = PostProcessString(TextToOutput + GetParametersString(model, context), model, context);
+            if (OutputEntireMessageKeepingColors)
             {
-                string str = PostProcessString(TextToOutput + GetParametersString(model, context), model, context);
-                if (OutputEntireMessageKeepingColors)
-                {
-                    model.PushMessageToConveyor(new OutputToAdditionalWindowMessage(str) { SkipTriggers = true, SkipSubstitution = true, SkipHighlight = true });
-                }
-                else
-                {
-                    model.PushMessageToConveyor(new OutputToAdditionalWindowMessage(str, TextColor, BackgroundColor) { SkipTriggers = true, SkipSubstitution = true, SkipHighlight = true });
-                }
+                model.PushMessageToConveyor(new OutputToAdditionalWindowMessage(str) { SkipTriggers = true, SkipSubstitution = true, SkipHighlight = true });
             }
+            else
+            {
+                model.PushMessageToConveyor(new OutputToAdditionalWindowMessage(str, TextColor, BackgroundColor) { SkipTriggers = true, SkipSubstitution = true, SkipHighlight = true });
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="rootModel"></param>
+        /// <returns></returns>
+        [NotNull]
+        protected override string ReplaceVariables([NotNull] string input, [NotNull] RootModel rootModel)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int lastPos = 0;
+            int i = 0;
+            while (i < input.Length)
+            {
+                if (input[i] == '$')
+                {
+                    if (i - lastPos > 0)
+                        sb.Append(input, lastPos, i - lastPos);
+
+                    i++;
+                    int startPos = i;
+
+                    while (i < input.Length && char.IsLetterOrDigit(input[i]))
+                        i++;
+
+                    sb.Append(rootModel.GetVariableValue(input.Substring(startPos, i - startPos)));
+                    lastPos = i;
+                }
+
+                i++;
+            }
+
+            if (lastPos < input.Length)
+                sb.Append(input, lastPos, input.Length - lastPos);
+
+            return sb.ToString();
+        }
+
+        protected override string ReplaceParameters(string input, ActionExecutionContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            int lastPos = 0;
+            int i = 0;
+            while (i < input.Length - 1)
+            {
+                if (input[i] == '%')
+                {
+                    if (i < input.Length - 2 && input[i + 1] == '%' && char.IsDigit(input[i + 2]))
+                    {
+                        if (i - lastPos > 0)
+                            sb.Append(input, lastPos, i - lastPos);
+
+                        sb.Append(GetParameter((int)Char.GetNumericValue(input[i + 2]), context));
+
+                        lastPos = i + 3;
+                        i += 2;
+                    }
+                    else if (char.IsDigit(input[i + 1]))
+                    {
+                        if (i - lastPos > 0)
+                            sb.Append(input, lastPos, i - lastPos);
+
+                        sb.Append(GetParameter((int)Char.GetNumericValue(input[i + 1]), context));
+                        lastPos = i + 2;
+                        i++;
+                    }
+                }
+
+                i++;
+            }
+
+            if (lastPos < input.Length)
+                sb.Append(input, lastPos, input.Length - lastPos);
+
+            return sb.ToString();
         }
 
         /// <summary>
